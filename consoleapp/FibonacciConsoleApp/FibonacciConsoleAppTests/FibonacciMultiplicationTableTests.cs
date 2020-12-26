3fbcdf21450f0dea0983cff6cbd57a339e21d28a
Angel Dimitrov <angel.s.dimitrov@gmail.com>
2020-12-26 13:59:34 -0800

    - initial commit for console app, created the project - adding unit tests for testing Fibonacci sequence and Fibonacci multiplication table - methods for getting Fibonacci sequence and multiplication table will be static in CustomMath.cs - Fibonacci sequence should be get by 2 overloads - 1) by just giving the length of the sequence and using default first two values 0 and 1 and 2) by providing first two values to the method and the length of the sequence - Fibonacci multiplication table will work the same way

---
+++ consoleapp/FibonacciConsoleApp/FibonacciConsoleAppTests/FibonacciMultiplicationTableTests.cs
@@ -0,0 +1,88 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace FibonacciMultiplicationTableConsoleAppTests
+{
+    [TestClass]
+    public class FibonacciMultiplicationTableTests
+    {
+        [TestMethod]
+        public void FibonacciMultiplicationTableZeroElements()
+        {
+            double first = 0;
+            double second = 1;
+            int sequenceLength = 0;
+            List<List<double>> fibonacciMultiplicationTableActual = new List<List<double>>();
+            fibonacciMultiplicationTableActual = GetFibonacciMultiplicationTable(first, second, sequenceLength);
+
+            Assert.AreEqual(0, fibonacciMultiplicationTableActual.Count, 0, "Count of Fibonacci multiplication table with 0 elements is more than 0. Actual count " + fibonacciMultiplicationTableActual.Count);
+        }
+
+        [TestMethod]
+        public void FibonacciMultiplicationTableZeroElementsDefaults()
+        {
+            int sequenceLength = 0;
+            List<List<double>> fibonacciMultiplicationTableActual = new List<List<double>>();
+            fibonacciMultiplicationTableActual = GetFibonacciMultiplicationTable(sequenceLength);
+
+            Assert.AreEqual(0, fibonacciMultiplicationTableActual.Count, 0, "Count of Fibonacci multiplication table defaults with 0 elements is more than 0. Actual count " + fibonacciMultiplicationTableActual.Count);
+        }
+
+        [TestMethod]
+        public void FibonacciMultiplicationTableFiveElements()
+        {
+            double first = 1;
+            double second = 1;
+            int sequenceLength = 5;
+            List<List<double>> fibonacciMultiplicationTableActual = new List<List<double>>();
+            fibonacciMultiplicationTableActual = GetFibonacciMultiplicationTable(first, second, sequenceLength);
+
+            List<List<double>> fibonacciMultiplicationTableExpected = new List<List<double>>()
+            { new List<double>(){1,1,2,3,5},
+            new List<double>(){1,1,1,1,1,1},
+            new List<double>(){1,2,2,3,4,6},
+            new List<double>(){2,3,3,4,5,7},
+            new List<double>(){3,5,5,7,9,13},
+            new List<double>(){5,8,8,11,14,20} };
+
+            for (int i = 0; i < fibonacciMultiplicationTableExpected.Count; i++)
+            {
+                List<double> currentExpected = fibonacciMultiplicationTableExpected[i];
+                List<double> currentActual = fibonacciMultiplicationTableActual[i];
+
+                CollectionAssert.AreEqual(currentExpected, currentActual,
+                "Expected result of Fibonacci multiplication table with 5 elements is not correct at row " + i + ". " +
+                "Expected <" + String.Join(", ", currentExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", currentActual.ToArray()) + ">");
+            }
+        }
+
+        [TestMethod]
+        public void FibonacciMultiplicationTableFiveElementsDefaults()
+        {
+            int sequenceLength = 5;
+            List<List<double>> fibonacciMultiplicationTableActual = new List<List<double>>();
+            fibonacciMultiplicationTableActual = GetFibonacciMultiplicationTable(sequenceLength);
+
+            List<List<double>> fibonacciMultiplicationTableExpected = new List<List<double>>()
+            { new List<double>(){0,1,1,2,3},
+            new List<double>(){0,0,0,0,0,0},
+            new List<double>(){1,0,1,1,2,3},
+            new List<double>(){1,0,1,1,2,3},
+            new List<double>(){2,0,2,2,4,6},
+            new List<double>(){3,0,3,3,6,9} };
+
+            for (int i = 0; i < fibonacciMultiplicationTableExpected.Count; i++)
+            {
+                List<double> currentExpected = fibonacciMultiplicationTableExpected[i];
+                List<double> currentActual = fibonacciMultiplicationTableActual[i];
+
+                CollectionAssert.AreEqual(currentExpected, currentActual,
+                "Expected result of Fibonacci multiplication table with 5 elements defaults is not correct at row " + i + ". " +
+                "Expected <" + String.Join(", ", currentExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", currentActual.ToArray()) + ">");
+            }
+        }
+    }
+}
---
+++ consoleapp/FibonacciConsoleApp/FibonacciConsoleAppTests/FibonacciTests.cs
@@ -0,0 +1,161 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CustomMath;
+using System.Collections.Generic;
+using System;
+
+namespace FibonacciMultiplicationTableConsoleAppTests
+{
+    [TestClass]
+    public class FibonacciTests
+    {
+        [TestMethod]
+        public void FibonacciZeroElements()
+        {
+            double first = 0;
+            double second = 1;
+            int sequenceLength = 0;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(first, second, sequenceLength);
+
+            Assert.AreEqual(0, fibonacciSequenceActual.Count, 0, "Count of Fibonacci sequence with 0 elements is more than 0. Actual count " + fibonacciSequenceActual.Count);
+        }
+
+        [TestMethod]
+        public void FibonacciZeroElementsDefaults()
+        {
+            int sequenceLength = 0;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(sequenceLength);
+
+            Assert.AreEqual(0, fibonacciSequenceActual.Count, 0, "Count of Fibonacci sequence defaults with 0 elements is more than 0. Actual count " + fibonacciSequenceActual.Count);
+        }
+
+        [TestMethod]
+        public void FibonacciOneElement()
+        {
+            double first = 0;
+            double second = 1;
+            int sequenceLength = 1;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(first, second, sequenceLength);
+
+            List<double> fibonacciSequenceExpected = new List<double>() {0};
+
+            CollectionAssert.AreEqual(fibonacciSequenceExpected, fibonacciSequenceActual, 
+                "Expected result of Fibonacci sequence with 1 element is not correct. " +
+                "Expected <" + String.Join(", ", fibonacciSequenceExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", fibonacciSequenceActual.ToArray()) + ">");
+        }
+
+        [TestMethod]
+        public void FibonacciOneElementDefaults()
+        {
+            int sequenceLength = 1;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(sequenceLength);
+
+            List<double> fibonacciSequenceExpected = new List<double>() { 0 };
+
+            CollectionAssert.AreEqual(fibonacciSequenceExpected, fibonacciSequenceActual,
+                "Expected result of Fibonacci sequence defaults with 1 element is not correct. " +
+                "Expected <" + String.Join(", ", fibonacciSequenceExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", fibonacciSequenceActual.ToArray()) + ">");
+        }
+
+        [TestMethod]
+        public void FibonacciTwoElements()
+        {
+            double first = 0;
+            double second = 1;
+            int sequenceLength = 2;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(first, second, sequenceLength);
+
+            List<double> fibonacciSequenceExpected = new List<double>() { 0, 1 };
+
+            CollectionAssert.AreEqual(fibonacciSequenceExpected, fibonacciSequenceActual,
+                "Expected result of Fibonacci sequence with 2 elements is not correct. " +
+                "Expected <" + String.Join(", ", fibonacciSequenceExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", fibonacciSequenceActual.ToArray()) + ">");
+        }
+
+        [TestMethod]
+        public void FibonacciTwoElementsDefaults()
+        {
+            int sequenceLength = 2;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(sequenceLength);
+
+            List<double> fibonacciSequenceExpected = new List<double>() { 0, 1 };
+
+            CollectionAssert.AreEqual(fibonacciSequenceExpected, fibonacciSequenceActual,
+                "Expected result of Fibonacci sequence defaults with 2 elements is not correct. " +
+                "Expected <" + String.Join(", ", fibonacciSequenceExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", fibonacciSequenceActual.ToArray()) + ">");
+        }
+
+        [TestMethod]
+        public void FibonacciTenElements()
+        {
+            double first = 0;
+            double second = 1;
+            int sequenceLength = 10;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(first, second, sequenceLength);
+
+            List<double> fibonacciSequenceExpected = new List<double>() { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+            CollectionAssert.AreEqual(fibonacciSequenceExpected, fibonacciSequenceActual,
+                "Expected result of Fibonacci sequence with 10 elements is not correct. " +
+                "Expected <" + String.Join(", ", fibonacciSequenceExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", fibonacciSequenceActual.ToArray()) + ">");
+        }
+
+        [TestMethod]
+        public void FibonacciTenElementsDefaults()
+        {
+            int sequenceLength = 10;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(sequenceLength);
+
+            List<double> fibonacciSequenceExpected = new List<double>() { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34 };
+
+            CollectionAssert.AreEqual(fibonacciSequenceExpected, fibonacciSequenceActual,
+                "Expected result of Fibonacci sequence defaults with 10 elements is not correct. " +
+                "Expected <" + String.Join(", ", fibonacciSequenceExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", fibonacciSequenceActual.ToArray()) + ">");
+        }
+
+        [TestMethod]
+        public void FibonacciFiftyElements()
+        {
+            double first = 0;
+            double second = 1;
+            int sequenceLength = 50;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(first, second, sequenceLength);
+
+            List<double> fibonacciSequenceExpected = new List<double>() {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049 };
+
+            CollectionAssert.AreEqual(fibonacciSequenceExpected, fibonacciSequenceActual,
+                "Expected result of Fibonacci sequence with 50 elements is not correct. " +
+                "Expected <" + String.Join(", ", fibonacciSequenceExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", fibonacciSequenceActual.ToArray()) + ">");
+        }
+
+        [TestMethod]
+        public void FibonacciFiftyElementsDefaults()
+        {
+            int sequenceLength = 50;
+            List<double> fibonacciSequenceActual = new List<double>();
+            fibonacciSequenceActual = GetFibonacciSequence(sequenceLength);
+
+            List<double> fibonacciSequenceExpected = new List<double>() { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155, 165580141, 267914296, 433494437, 701408733, 1134903170, 1836311903, 2971215073, 4807526976, 7778742049 };
+
+            CollectionAssert.AreEqual(fibonacciSequenceExpected, fibonacciSequenceActual,
+                "Expected result of Fibonacci sequence defaults with 50 elements is not correct. " +
+                "Expected <" + String.Join(", ", fibonacciSequenceExpected.ToArray()) + ">. " +
+                "Actual <" + String.Join(", ", fibonacciSequenceActual.ToArray()) + ">");
+        }
+    }
+}
