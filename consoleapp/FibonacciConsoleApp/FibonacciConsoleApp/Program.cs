3d5db3135e11fee0e63cb35735d899dd9fcd462f
Angel Dimitrov <angel.s.dimitrov@gmail.com>
2020-12-27 01:09:12 -0800

    - added reading the size of the Fibonacci sequence table from the console - added displaying the Fibonacci sequence table to the console

--- consoleapp/FibonacciConsoleApp/FibonacciConsoleApp/Program.cs
+++ consoleapp/FibonacciConsoleApp/FibonacciConsoleApp/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using CustomMath;
 
 namespace FibonacciMultiplicationTableConsoleApp
 {
@@ -6,7 +8,38 @@ namespace FibonacciMultiplicationTableConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            while (true)
+            {
+                Console.WriteLine("Enter a number for the size of the Fibonacci multiplication table");
+
+                string userInput = Console.ReadLine();
+                int size;
+
+                if (Int32.TryParse(userInput, out size))
+                {
+                    if (size == 0)
+                    {
+                        Console.WriteLine("No result for size of 0");
+                    }
+                    else
+                    {
+                        List<List<double>> multiplicationTable = Fibonacci.GetMultiplicationTable(size);
+                        Console.WriteLine("  " + String.Join(" ", multiplicationTable[0].ToArray()));
+                        for (int i = 1; i < multiplicationTable.Count; i++)
+                        {
+                            Console.WriteLine(String.Join(" ", multiplicationTable[i].ToArray()));
+                        }
+                    }
+                   
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+
+                Console.WriteLine();
+            }
+
         }
     }
 }
