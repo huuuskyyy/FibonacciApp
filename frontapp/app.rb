3c90f8a7377c384475079476532023a317b19131
Angel Dimitrov <angel.s.dimitrov@gmail.com>
2021-01-02 12:23:13 -0800

    - adding get and post action for multiplication table - both are executed on form submit within the html

---
+++ frontapp/app.rb
@@ -0,0 +1,48 @@
+require 'sinatra'
+require 'net/http'
+require 'uri'
+require 'json'
+
+set :public_folder, 'public'
+
+get '/fibonacci' do
+  table = nil
+  size = params[:n];
+  
+  if size != nil
+	uri = URI.parse("http://localhost:44331/fibonacci/" + size)
+	http = Net::HTTP.new(uri.host, uri.port)
+	request = Net::HTTP::Get.new(uri.request_uri)
+
+	response = http.request(request)
+	
+	if response.kind_of? Net::HTTPSuccess 
+		table = JSON.parse(response.body)
+		if table.count > 0
+			table[0].insert(0, nil)
+		end
+	end
+  end
+	
+  erb :fibonacci, locals: {size: params[:n], table: table, saved: false}, layout: :application
+end
+
+post '/fibonacci' do
+  table = nil   
+  saved = false 
+  size = nil
+	
+	if params[:size] != nil
+		uri = URI('http://localhost:44331/fibonacci')
+		response = Net::HTTP.post_form(uri, 'Size' => params[:size])
+		if response.kind_of? Net::HTTPSuccess 
+			table = JSON.parse(response.body)
+			if table.count > 0
+				table[0].insert(0, nil)
+				saved = true
+			end
+		end
+	end
+
+  erb :fibonacci, locals: {size: size, table: table, saved: saved}, layout: :application
+end
